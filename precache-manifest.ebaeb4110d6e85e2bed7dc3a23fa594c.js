99c91e1eeb3e7e158140de284019b81525f2c36e
alls7554 <alls970459@gmail.com>
2021-10-28 11:51:32 +0900

    Updates

---
+++ precache-manifest.ebaeb4110d6e85e2bed7dc3a23fa594c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477ffeb799790ffe5135a2e002ced885",
+    "url": "/clone_twitter/index.html"
+  },
+  {
+    "revision": "ab8afb052a3b7a8c5975",
+    "url": "/clone_twitter/static/css/main.0e30bf6d.chunk.css"
+  },
+  {
+    "revision": "a80b360edb2b2532c31b",
+    "url": "/clone_twitter/static/js/2.61fe75dc.chunk.js"
+  },
+  {
+    "revision": "beedd4aa548457aec04236a3c132a394",
+    "url": "/clone_twitter/static/js/2.61fe75dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8afb052a3b7a8c5975",
+    "url": "/clone_twitter/static/js/main.967f9fb5.chunk.js"
+  },
+  {
+    "revision": "850a87389f6bbc148e94",
+    "url": "/clone_twitter/static/js/runtime-main.2be3dd56.js"
+  }
+]);
