6217e0c8a1bc551c6e26efcad0e81d7cfffda581
alls7554 <alls970459@gmail.com>
2021-10-28 10:22:24 +0900

    Update Profile & Bug Fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,30 @@ import { onAuthStateChanged } from '@firebase/auth';
 function App() {
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
-
+  
   useEffect(() => {
     const auth = authService.getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        setUserObj(user);
+        setUserObj({
+          uid: user.uid,
+          displayName: user.displayName
+        })
       }
       setInit(true);
     });
   }, []);
 
+  const refreshUser = () => {
+    setUserObj({
+      uid: authService.getAuth().currentUser.uid,
+      displayName: authService.getAuth().currentUser.displayName
+    })
+  }
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} refreshUser={refreshUser} /> : "Initializing..."}
       {/* <footer>&copy; {new Date().getFullYear()} Cwitter</footer> */}
     </>
   ) 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { authService } from 'mfBase';
 import { useHistory } from 'react-router';
+// import { collection, getDocs, orderBy, query, where } from '@firebase/firestore';
+import { updateProfile } from '@firebase/auth';
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
   
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
   const onLogOutClick = () => {
-
     const auth = authService.getAuth();
     authService.signOut(auth);
     history.push('/');
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(authService.getAuth().currentUser, { displayName: newDisplayName });
+    }
+
+    refreshUser();
+  }
+
+  const onChange = e => {
+    const { target: { value } } = e;
+    setNewDisplayName(value);
+  }
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='Display Name'
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input
+          type="submit"
+          value='Update Profile'
+        />
+      </form>
       <button onClick={onLogOutClick}>Sign Out</button>
     </>
   );
@@ -23,3 +51,23 @@ const Profile = () => {
 
 
 export default Profile;
+
+
+  // const getMyCweets = async () => {
+
+  //   const q = query(
+  //     collection(dbService, "cweets"),
+  //     where("creatorId", "==", userObj.uid),
+  //     orderBy('createdAt', 'desc')
+  //   );
+
+  //   const querySnapshot = await getDocs(q);
+  //   querySnapshot.forEach((doc) => {
+  //     // doc.data() is never undefined for query doc snapshots
+  //     console.log(doc.id, " => ", doc.data());
+  //   });
+  // }
+
+  // useEffect(() => {
+  //   getMyCweets();
+  // }, []);
